cc911dd13265be0546256060129161a12bf861bd
oliver-yeung <oliver_yeung@163.com>
2019-12-05 19:51:29 +0800

    second

---
+++ springBoot/src/main/java/com/oliveryn/Application.java
@@ -0,0 +1,12 @@
+package com.oliveryn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    //springBoot项目通过此main函数启动
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
